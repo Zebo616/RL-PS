fa0f54b00b7c0d18f9dfd5ac0b27604f9668b519
Zebo616 <stevio616@gmail.com>
2022-05-13 19:55:49 -0500

    added more images

--- graph-time.py
+++ graph-time.py
@@ -14,25 +14,35 @@ if __name__ == "__main__":
     f.close()
 
     x1 = []
+    Dtimes1 = []
+    previous = 0
 
     for i in range(len(times1)):
         times1[i] = float(times1[i][:-2])
+        Dtimes1.append(times1[i] - previous)
         x1.append(i)
+        previous = times1[i]
 
     x2 = []
+    Dtimes2 = []
+    previous = 0
     
     for i in range(len(times2)):
         times2[i] = float(times2[i][:-2])
+        Dtimes2.append(times2[i] - previous)
         x2.append(i)
+        previous = times2[i]
 
     for i in range(len(times1)):
         if i == len(times1) or i == len(times2):
             break
         print("difference in training time is:", times2[i] - times1[i])
 
-    plt.plot(x2, times2, label = file2)
+    plt.plot(x2, Dtimes2, label = file2)
+    plt.plot(x1, Dtimes1, label = file1)
 
-    plt.plot(x1, times1, label = file1)
+    # plt.plot(x2, times2, label = file2)
+    # plt.plot(x1, times1, label = file1)
 
     # naming the x axis
     plt.xlabel('iteration number')
@@ -40,7 +50,7 @@ if __name__ == "__main__":
     plt.ylabel('time')
 
     # giving a title to my graph
-    plt.title('Training Time')
+    plt.title('Training Time, Per Timestep')
 
     plt.legend(loc='center left', bbox_to_anchor=(1, 0))
 
